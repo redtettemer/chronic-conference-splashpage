fbfc9f386b4b683e7c9973693e810110c3a2fcbd
Ben Barclay <bbarclay@rtop.com>
2018-01-17 11:13:20 -0500

    add api

--- config/application.rb
+++ config/application.rb
@@ -66,9 +66,7 @@ module ChronicConference
     config.assets.version = '1.0'
 
     ActionMailer::Base.smtp_settings = {
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :domain => ENV['SENDGRID_DOMAIN'],
+        :sengrid_api_key => ENV['SENDGRID_API_KEY'],
         :address => 'smtp.sendgrid.net',
         :port => 587,
         :authentication => :plain,
