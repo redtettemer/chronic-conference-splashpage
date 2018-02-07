2d8ac04fb5a40216a598bfcbc21a98a5809d0778
sarah-driban <sdriban@rtop.com>
2018-02-07 14:52:13 -0500

    removing form and updating button to link out to eventbrite

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ ChronicConference::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'home#index', as: :home
-  post '/submit', to: 'home#submit'
-  get '/background', to: 'home#backgroundtest'
   # get '/columns', to: 'home#columns'
 end
